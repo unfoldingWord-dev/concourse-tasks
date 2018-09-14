621ae4fa9c34c7459426b52774d7f39bb22fc1a2
Joel Lonbeck <joel@neutrinographics.com>
2018-09-14 12:41:46 -0700

    test

--- scripts/run-add-build-comment.sh
+++ scripts/run-add-build-comment.sh
@@ -9,13 +9,8 @@ set -xe
 echo $text >> build_meta/comment.md
 echo $text >> build_meta/notification.txt
 # TODO: debugging
-ls -la code-base/.git
-cat code-base/.git/HEAD
-cat code-base/.git/FETCH_HEAD
-cat code-base/.git/commit_message
-cat code-base/.git/committer
 COMMIT=$(cat code-base/.git/ref)
 cd code-base/
 git branch --contains $COMMIT
-git reflog show --all | grep $COMMIT
+# git reflog show --all | grep $COMMIT
 git name-rev $COMMIT
