00189d245eae11a4401c0e3c7d03fd3348bc7d5a
Joel Lonbeck <joel@neutrinographics.com>
2018-09-14 12:49:23 -0700

    test

--- scripts/run-add-build-comment.sh
+++ scripts/run-add-build-comment.sh
@@ -5,11 +5,14 @@ set -xe
 # copy input to output
 ./concourse/scripts/copy-dir.sh input_meta build_meta
 
-# append new values
-echo $text >> build_meta/comment.md
-echo $text >> build_meta/notification.txt
-# TODO: debugging
+# read branch name
 cd code-base/
 COMMIT=$(cat .git/ref)
 BRANCH=$(git name-rev $COMMIT | awk '{print $2;}')
 echo $BRANCH
+
+echo "${text//BRANCH/$BRANCH}"
+
+# append new values
+echo $text >> build_meta/comment.md
+echo $text >> build_meta/notification.txt
