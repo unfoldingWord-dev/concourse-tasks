3f0e2e805beaf414a3c723fa44f89ca7b00dae60
Joel Lonbeck <joel@neutrinographics.com>
2018-09-18 16:11:02 -0700

    moved builds into global build dir

--- scripts/run-build-linux.sh
+++ scripts/run-build-linux.sh
@@ -5,7 +5,7 @@ set -xe
 FILE=translationCore-linux-x64-$VERSION-$HASH.zip
 
 ./node_modules/.bin/gulp build --linux || exit 1;
-./node_modules/.bin/gulp release-linux --out=../build/linux/$BRANCH/$FILE || exit 1;
+./node_modules/.bin/gulp release-linux --out=../build/$BRANCH/$FILE || exit 1;
 
 echo "[Linux build $VERSION ($HASH)]($BUILD_CDN/$FILE) is ready for download." >> ../build_meta/comment.md
 echo "<$BUILD_CDN/$BRANCH/$FILE|Linux build $HASH (v$VERSION)> is ready for download." >> ../build_meta/notification.txt
