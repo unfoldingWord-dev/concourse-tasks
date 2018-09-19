180931b2b1f98e21615c8c9ac764d4302de39a38
Joel Lonbeck <joel@neutrinographics.com>
2018-09-18 18:18:33 -0700

    fixed build links for pull requests

--- scripts/run-build-linux.sh
+++ scripts/run-build-linux.sh
@@ -7,5 +7,5 @@ FILE=translationCore-linux-x64-$VERSION-$HASH.zip
 ./node_modules/.bin/gulp build --linux || exit 1;
 ./node_modules/.bin/gulp release-linux --out=../build/$SAFE_BRANCH/$FILE || exit 1;
 
-echo "[Linux build $VERSION ($HASH)]($BUILD_CDN/$FILE) is ready for download." >> ../build_meta/comment.md
+echo "[Linux build $VERSION ($HASH)]($BUILD_CDN/build/$SAFE_BRANCH/$FILE) is ready for download." >> ../build_meta/comment.md
 echo "<$BUILD_CDN/build/$SAFE_BRANCH/$FILE|Linux build $HASH (v$VERSION)> is ready for download." >> ../build_meta/notification.txt
--- scripts/run-build-windows.sh
+++ scripts/run-build-windows.sh
@@ -10,8 +10,8 @@ FILE32=translationCore-win-x32-$VERSION-$HASH.setup.exe
 ./node_modules/.bin/gulp release-win32 --out=../build/$SAFE_BRANCH/$FILE32 || exit 1;
 
 # x64
-echo "[Windows x64 build $VERSION ($HASH)]($BUILD_CDN/$FILE64) is ready for download." >> ../build_meta/comment.md
+echo "[Windows x64 build $VERSION ($HASH)]($BUILD_CDN/build/$SAFE_BRANCH/$FILE64) is ready for download." >> ../build_meta/comment.md
 echo "<$BUILD_CDN/build/$SAFE_BRANCH/$FILE64|Windows x64 build $HASH (v$VERSION)> is ready for download." >> ../build_meta/notification.txt
 # x32
-echo "[Windows x32 build $VERSION ($HASH)]($BUILD_CDN/$FILE32) is ready for download." >> ../build_meta/comment.md
+echo "[Windows x32 build $VERSION ($HASH)]($BUILD_CDN/build/$SAFE_BRANCH/$FILE32) is ready for download." >> ../build_meta/comment.md
 echo "<$BUILD_CDN/build/$SAFE_BRANCH/$FILE32|Windows x32 build $HASH (v$VERSION)> is ready for download." >> ../build_meta/notification.txt
