8755fc42c3640bcf56c51244e2be8a4c85c374a0
Joel Lonbeck <joel@neutrinographics.com>
2018-09-21 13:18:04 -0700

    updated linux builds to produce deb packages

--- scripts/run-build-linux.sh
+++ scripts/run-build-linux.sh
@@ -2,10 +2,10 @@
 
 set -xe
 
-FILE=translationCore-linux-x64-$VERSION-$HASH.zip
+FILE=translationCore-linux-x64-$VERSION-$HASH.deb
 
 ./node_modules/.bin/gulp build --linux || exit 1;
-./node_modules/.bin/gulp release-linux --out=../build/$SAFE_BRANCH/$FILE || exit 1;
+./node_modules/.bin/gulp release-linux-deb --out=../build/$SAFE_BRANCH/$FILE || exit 1;
 
 echo "[Linux build $VERSION ($HASH)]($BUILD_CDN/build/$SAFE_BRANCH/$FILE) is ready for download." >> ../build_meta/comment.md
 echo "<$BUILD_CDN/build/$SAFE_BRANCH/$FILE|Linux build $HASH (v$VERSION)> is ready for download." >> ../build_meta/notification.txt
