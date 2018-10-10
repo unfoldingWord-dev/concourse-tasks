659227696510b90b2713348cd4db53f5fa069c47
Joel Lonbeck <joel@neutrinographics.com>
2018-10-10 14:19:32 -0700

    added script to update langnames

--- scripts/run-build.sh
+++ scripts/run-build.sh
@@ -8,18 +8,23 @@ set -xe
 # read branch name
 BRANCH=$(./concourse/scripts/get-branch-name.sh code-base)
 
-# filename safe branch
+# branch name with unsafe characters removed
 SAFE_BRANCH=$(echo "$BRANCH" | sed -e 's/[^A-Za-z0-9._-]/_/g')
 
 cd code-base/
 
+# read build meta
 HASH=$(git rev-parse HEAD | cut -c1-7)
 VERSION=$(node -p -e "require('./package.json').version")
 
+# Prepare environment variables
 # TODO: it would be nicer to decrypt .env.enc instead
 echo "TC_HELP_DESK_TOKEN=$TC_HELP_DESK_TOKEN" >> .env
 echo "TC_HELP_DESK_EMAIL=$TC_HELP_DESK_EMAIL" >> .env
 echo "BUILD=$HASH" >> .env
+
+# run pre-build content updates
+../concourse/scripts/update-content.sh
 
 # execute the appropriate platform builder
 HASH=$HASH VERSION=$VERSION BUILD_CDN=$BUILD_CDN BRANCH=$BRANCH SAFE_BRANCH=$SAFE_BRANCH ../concourse/scripts/run-build-$PLATFORM.sh
---
+++ scripts/update-content.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This script performs content updates for translationCore
+
+set -xe
+
+echo "Downloading latest language names from translationDatabase..."
+curl https://td.unfoldingword.org/exports/langnames.json > src/assets/langnames.json
